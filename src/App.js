af72f4ac38cfe79f2893c7f4dfa687e9e8d98a2f
Jose Medina <yanoya10@hotmail.com>
2020-05-25 18:29:07 -0400

    name change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Darkmode from 'darkmode-js';
+
+new Darkmode().showWidget();
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          José Carlos Medina's react portfolio is coming - check back soon!
         </p>
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
