d49b517e02cd89189cdc6cd0f5c8977fbe2fb2da
Jose Medina <yanoya10@hotmail.com>
2020-07-01 13:18:24 -0400

    contact me revisions

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -15,14 +15,14 @@ function Contact(){
          <h2 className ="title">Contact</h2>
           <Row className="work">
              <Col>
-              <h5 className="work"> I'm available for work opportunities, part-time and full-time, projects - and to play tennis also - please drop me a note!</h5>
+              <h5 className="work"> I'm available for work opportunities, part-time and full-time, projects - and to play tennis also!</h5>
              </Col>              
           </Row>
           <br></br>
           <Row className="work">
              <Col>
               <img src={email} height="50px" alt="email" className="image"/>
-              <h6><b><a href="mailto:jose.c.medina@me.com" target="_blank" className="contactMe">E-mail</a></b></h6>
+              <h6><b>Drop me a note! - <a href="mailto:jose.c.medina@me.com" target="_blank" className="contactMe">E-mail</a></b></h6>
              </Col>                       
           </Row>
           <br></br>
@@ -36,14 +36,14 @@ function Contact(){
           <Row className="work">
              <Col>
               <img src={linkedin} height="50px" alt="linkedin" className="image"/>
-              <h6><b>Connect with me - <a href="https://www.linkedin.com/in/josecmedinajr/" target="_blank" className="contactMe">LinkedIn</a></b></h6>
+              <h6><b>Let's connect - <a href="https://www.linkedin.com/in/josecmedinajr/" target="_blank" className="contactMe">LinkedIn</a></b></h6>
              </Col>               
           </Row>
           <br></br>
           <Row className="work">
              <Col>
              <img src={twitter} height="50px" alt="twitter" />
-              <h6><b>Follow me on - <a href="https://twitter.com/JCMLV" target="_blank" className="contactMe">Twitter</a></b></h6>
+              <h6><b>Follow me - <a href="https://twitter.com/JCMLV" target="_blank" className="contactMe">Twitter</a></b></h6>
              </Col>              
           </Row>
             </Container>
